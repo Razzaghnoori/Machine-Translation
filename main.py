eeb98f6cdbd48ee2f8527bf71a4303e0c7aca45c
Newsha Ozgoli <newshaozgoli@gmail.com>
2019-08-09 21:15:06 +0200

    Works

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
+
+import re
 import string
+import numpy  as np
+import argparse
+
 from pickle import load
 from numpy import array
+from math import floor
+from sklearn.model_selection import train_test_split
+from keras import backend as K
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.callbacks import ModelCheckpoint
-from math import floor
 from keras.layers import LSTM
 from keras.layers import Dense
 from keras.layers import Embedding
 from keras.layers import RepeatVector
 from keras.layers import TimeDistributed
 from keras.utils.vis_utils import plot_model
+from keras.models import load_model
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--train', '-t', help='Train if set.', action='store_true')
+parser.add_argument('--predict', '-p', help='Predict if set.', action='store_true')
+
+arguments = parser.parse_args()
 
 # fit a tokenizer
-def create_tokenizer(lines):
+def create_tokenizer(txt):
     tokenizer = Tokenizer(oov_token='UNK')
-    tokenizer.fit_on_texts(lines)
+    tokenizer.fit_on_texts(txt)
     return tokenizer
 
-# max sentence length
-def max_length(lines):
-    return max(len(line.split()) for line in lines)
-
 #reading file and split it into sentences
-import re
 def read_file(filename):
     with open(filename) as file:
         text = file.read()
@@ -37,11 +46,11 @@ def read_file(filename):
     return result.strip().split('\n')
 
 # encode and pad sequences
-def encode_sequences(tokenizer, lines):
+def encode_sequences(tokenizer, lines, max_len=30):
     # integer encode sequences
     X = tokenizer.texts_to_sequences(lines)
     # pad sequences with 0 values
-    X = pad_sequences(X, padding='post')
+    X = pad_sequences(X, maxlen=max_len, padding='post')
     return X
 
 # one hot encode target sequence
@@ -54,60 +63,93 @@ def encode_output(sequences, vocab_size):
     y = y.reshape(sequences.shape[0], sequences.shape[1], vocab_size)
     return y
 
-#split data to train and test
-def get_training_and_testing_sets(file_list):
-    split = 0.9
-    split_index = floor(len(file_list) * split)
-    training = file_list[:split_index]
-    testing = file_list[split_index:]
-    return training, testing
-
-
-# model
-def define_model(src_vocab, tar_vocab, src_timesteps, tar_timesteps, n_units):
+def define_model(X, y, tar_vocab_size, n_units):
     model = Sequential()
-    model.add(Embedding(src_vocab, n_units, input_length=src_timesteps, mask_zero=True))
+    model.add(Embedding(K.np.max(X)+1, n_units, input_length=X.shape[1]))
     model.add(LSTM(n_units))
-    model.add(RepeatVector(tar_timesteps))
+    model.add(RepeatVector(y.shape[1]))
     model.add(LSTM(n_units, return_sequences=True))
-    model.add(TimeDistributed(Dense(tar_vocab, activation='softmax')))
+    model.add(TimeDistributed(Dense(tar_vocab_size, activation='softmax')))
     return model
 
 def main():
-    # prepare english tokenizer
-    # open the file as read only
-    eng_lines = read_file('test')
-    eng_tokenizer = create_tokenizer(eng_lines)
-    eng_vocab_size = len(eng_tokenizer.word_index) + 1
-    eng_length = max_length(eng_lines)
+    with open('eng') as f:
+        eng_txt = f.readlines()
+    with open('fa') as f:
+        fa_txt = f.readlines()
 
-    #Persian Tokenizer
-    prs_lines = read_file('tt')
-    prs_tokenizer = create_tokenizer(prs_lines)
-    prs_vocab_size = len(prs_tokenizer.word_index) + 1
-    prs_length = max_length(prs_lines)
+    eng_tokenizer = create_tokenizer(eng_txt)
+    X = encode_sequences(eng_tokenizer, eng_txt)
+
+    fa_tokenizer = create_tokenizer(fa_txt)
+    y = encode_sequences(fa_tokenizer, fa_txt)
+    fa_vocab_size = K.np.max(y) + 1
+    y = to_categorical(y)
+
+    perm = np.random.permutation(X.shape[0])
+    X, y = X[perm], y[perm]
 
     # prepare training and testing data
-    eng_training, eng_testing = get_training_and_testing_sets(eng_lines)
-    eng_training = encode_sequences(eng_tokenizer, eng_training)
-    eng_testing = encode_sequences(eng_tokenizer, eng_testing)
-    prs_training, prs_testing = get_training_and_testing_sets(prs_lines)
-    prs_training = encode_sequences(prs_tokenizer, prs_lines)
-    prs_training = encode_output(prs_training, prs_vocab_size)
-    prs_testing = encode_sequences(prs_tokenizer, prs_lines)
-    prs_testing = encode_output(prs_testing, prs_vocab_size)
+    X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=0.1)
 
     # define model
-    model = define_model(prs_vocab_size, eng_vocab_size, prs_length, eng_length, 256)
+    model = define_model(X, y, fa_vocab_size, 256)
     model.compile(optimizer='adam', loss='categorical_crossentropy')
     
     # summarize defined model
     print(model.summary())
+
+    return model, X_tr, y_tr, X_te, y_te
     
+def train(model, X_tr, y_tr, model_path):
     # fit model
-    filename = 'model.h5'
-    checkpoint = ModelCheckpoint(filename, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-    model.fit(eng_training, prs_training, epochs=30, batch_size=1, validation_data=(eng_testing, prs_testing), callbacks=[checkpoint], verbose=2)
+    checkpoint = ModelCheckpoint(model_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+    model.fit(X_tr, y_tr, epochs=100, batch_size=8, validation_split=0.1, callbacks=[checkpoint], verbose=2)
+    model.save(model_path)
+
+def predict(model_path, eng_tokenizer, fa_tokenizer):
+    model = load_model(model_path)
+    
+    while True:
+        sents = [input()]
+        x = encode_sequences(eng_tokenizer, sents)
+
+        pred = model.predict(x)[0]
+        inds = pred.argmax(axis=-1)
+        print(' '.join([fa_tokenizer.index_word[x+1] for x in inds.tolist()]).replace(' UNK', ''))
+
 
 if __name__ == "__main__":
-    main()
+    model_path = 'model.h5'
+
+    with open('eng') as f:
+        eng_txt = f.readlines()
+    with open('fa') as f:
+        fa_txt = f.readlines()
+
+    eng_tokenizer = create_tokenizer(eng_txt)
+    X = encode_sequences(eng_tokenizer, eng_txt)
+
+    fa_tokenizer = create_tokenizer(fa_txt)
+    y = encode_sequences(fa_tokenizer, fa_txt)
+    fa_vocab_size = K.np.max(y) + 1
+    y = to_categorical(y)
+
+    perm = np.random.permutation(X.shape[0])
+    X, y = X[perm], y[perm]
+
+    # prepare training and testing data
+    X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=0.1)
+
+    # define model
+    model = define_model(X, y, fa_vocab_size, 256)
+    model.compile(optimizer='adam', loss='categorical_crossentropy')
+    
+    # summarize defined model
+    print(model.summary())
+
+    if arguments.train:
+        train(model, X_tr, y_tr, model_path)
+
+    if arguments.predict:
+        predict(model_path, eng_tokenizer, fa_tokenizer)
